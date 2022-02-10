b5be2a364ba1c3a7dfb4efb2d60732b263363f52
peter L <peter.l@uvaper.com>
2022-02-10 14:34:59 +0900

    리액트-네이티브-기초지식-1-컴보넌트

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
 import favicon from './assets/favicon.png';
 import main from './assets/windmill.jpg';
 import data from './data.json';
+import Card from './components/Card';
 
 import {
   Colors,
@@ -63,35 +64,8 @@ const App: () => Node = () => {
       </ScrollView>
       <View style={styles.cardContainer}>
         {/* 하나의 카드 영역을 나타내는 View */}
-        {/* --> i%2가 짝수일 경우 참항에 있는 JSX코드가 리턴(랜더링), i%2가 홀수일 경우 거짓항에 있는 JSX코드가 리턴(랜더링) */}
         {tip.map((content, i) => {
-          return i % 2 == 0 ? (
-            <View style={styles.cardEven} key={i}>
-              <Image style={styles.cardImage} source={{uri: content.image}} />
-              <View style={styles.cardText}>
-                <Text style={styles.cardTitle} numberOfLines={1}>
-                  {content.title}
-                </Text>
-                <Text style={styles.cardDesc} numberOfLines={3}>
-                  {content.desc}
-                </Text>
-                <Text style={styles.cardDate}>{content.date}</Text>
-              </View>
-            </View>
-          ) : (
-            <View style={styles.cardOdd} key={i}>
-              <Image style={styles.cardImage} source={{uri: content.image}} />
-              <View style={styles.cardText}>
-                <Text style={styles.cardTitle} numberOfLines={1}>
-                  {content.title}
-                </Text>
-                <Text style={styles.cardDesc} numberOfLines={3}>
-                  {content.desc}
-                </Text>
-                <Text style={styles.cardDate}>{content.date}</Text>
-              </View>
-            </View>
-          );
+          return <Card content={content} key={i} />;
         })}
       </View>
     </ScrollView>
@@ -176,38 +150,6 @@ const styles = StyleSheet.create({
   cardContainer: {
     marginTop: 10,
     marginLeft: 10,
-  },
-  card: {
-    flex: 1,
-    //컨텐츠들을 가로로 나열
-    //세로로 나열은 column <- 디폴트 값임
-    flexDirection: 'row',
-    margin: 10,
-    borderBottomWidth: 0.5,
-    borderBottomColor: '#eee',
-    paddingBottom: 10,
-  },
-  cardImage: {
-    flex: 1,
-    width: 100,
-    height: 100,
-    borderRadius: 10,
-  },
-  cardText: {
-    flex: 2,
-    flexDirection: 'column',
-    marginLeft: 10,
-  },
-  cardTitle: {
-    fontSize: 20,
-    fontWeight: '700',
-  },
-  cardDesc: {
-    fontSize: 15,
-  },
-  cardDate: {
-    fontSize: 10,
-    color: '#A6A6A6',
   },
 });
 
